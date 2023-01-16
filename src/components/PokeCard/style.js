b268d1d8a0555ea65ff84c5649ed44b166e35072
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-01-16 18:38:59 -0300

    :rocket: refatorando a exibição dos pokemons

--- src/components/PokeCard/style.js
+++ src/components/PokeCard/style.js
@@ -4,31 +4,45 @@ export const Container = styled.button`
     border: 0;
 
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 10px 40px;
-    gap: 2px;
+    padding: 10px 20px;
+    gap: 20px;
 
     cursor: pointer;
 
-    max-width: auto;
+    width: 300px;
+    max-width: 400px;
     height: auto;
 
-    background-color: #007FFF;
+    background-color: #F3F6F9;
 
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
     border-radius: 5px;
 
     font-family: 'Poppins';
     font-style: normal;
-    font-weight: 600;
-    font-size: 20px;
+    font-weight: 500;
+    font-size: 18px;
     line-height: 21px;
 
-    color: #FFFFFF;
+    color: #0A1929;
 
     margin: 2px;
 
-    flex-grow: 4;
+    flex-grow: 5;
+`
+
+export const StyledImg = styled.img`
+    width: 150px;
+    min-width: 150px;
+    max-width: 150px;
+    height: 150px;
+    min-height: 150px;
+    max-height: 150px;
+
+    object-fit: cover;
+
+    border-radius: 10px;
 `
